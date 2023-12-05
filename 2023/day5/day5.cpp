a21ef021d7dc2644cf083c76fc72c1aafa5002b3
Max Jordan <max.jordan1306@gmail.com>
2023-12-05 20:34:44 +0000

    Day 5 but faster

--- 2023/day5/day5.cpp
+++ 2023/day5/day5.cpp
@@ -1,6 +1,6 @@
 #include "../util/util.h"
 #include <unordered_map>
-
+#include <limits>
 
 using RangeMapping = std::vector<std::vector<std::pair<std::pair<long long, long long>, long long>>>;
 using SeedVector = std::vector<long long>;
@@ -46,8 +46,7 @@ std::pair<RangeMapping, SeedVector> buildMappingsAndSeeds(const std::vector<std:
 void part1(const std::vector<std::string>& lines) {
     auto [mapping, seeds] = buildMappingsAndSeeds(lines);
 
-    long long closestLocation = INT_MAX;
-    const int num_maps = mapping.size();
+    long long closestLocation = std::numeric_limits<long long>::max();
 
     long long currentPos;
     bool foundRange;
@@ -85,52 +84,77 @@ void part1(const std::vector<std::string>& lines) {
     std::cout << closestLocation << std::endl;
 }
 
+using SeedBounds = std::pair<long long, long long>;
+
 // Part 2 function
 void part2(const std::vector<std::string>& lines) {
 
     auto [mapping, seeds] = buildMappingsAndSeeds(lines);
+    std::vector<SeedBounds> seedBounds;
+    seedBounds.reserve(1000);
 
-    long long closestLocation = INT_MAX;
-    const int num_maps = mapping.size();
-
-    long long currentSeed;
-    bool foundRange;
-    int rangeIdx;
-    long long startSeed;
-    long long seedRange;
-
-    for (int seedIdx = 0; seedIdx <seeds.size(); seedIdx +=2) // Get the startSeed and ranges
+    for (int i = 0; i < seeds.size(); i += 2)
     {
-        startSeed = seeds[seedIdx];
-        seedRange = seeds[seedIdx + 1];
+        long long seed = seeds[i];
+        long long seedRange = seeds[i+1];
+        seedBounds.emplace_back(seed, seed+seedRange-1);
+    }
 
-        for (; startSeed < seeds[seedIdx] + seedRange; startSeed++) {
-            currentSeed = startSeed;
-            //std::cout << "Checking currentSeed " << currentSeed << std::endl;
-            for (const auto& map : mapping)
+    for (const auto& map : mapping) // This is layer by layer, e.g. seed-to-soil map, soil-to-fertilizer map etc
+    {
+        std::vector<SeedBounds> newSeedBounds;
+        for (const auto& seedBound : seedBounds) { // <3, 10> etc
+            const long long& seedLowerBound = seedBound.first; // 3
+            const long long& seedUpperBound = seedBound.second; // 10
+            bool foundSeedInSource = false;
+            for (const auto& sourceBound : map) // (<8,12>, 4) etc
             {
 
-                foundRange = false;
-                rangeIdx = 0;
-                while (rangeIdx < map.size() && !foundRange)
+                const auto& sourceLowerBound = sourceBound.first.first;
+                const auto& sourceUpperBound = sourceBound.first.second;
+                const auto& destOffset = sourceBound.second;
+                
+                if (seedLowerBound <= sourceLowerBound && seedUpperBound >= sourceUpperBound) // seedBound covers the entire range
                 {
-                    const std::pair<long long, long long>& sourceRange = map[rangeIdx].first;
-                    const long long& change = map[rangeIdx].second;
+                    newSeedBounds.emplace_back(seedLowerBound, sourceLowerBound-1);
+                    newSeedBounds.emplace_back(sourceLowerBound+destOffset, sourceUpperBound+destOffset);
+                    newSeedBounds.emplace_back(sourceUpperBound+1, seedUpperBound);
+                    foundSeedInSource = true;
 
-                    if (currentSeed >= sourceRange.first && currentSeed <= sourceRange.second)
-                    {
-                        foundRange = true;
-                        currentSeed += change;
-                        break;
-
-                    }
-                    rangeIdx++;
                 }
+                else if (seedLowerBound >= sourceLowerBound && seedUpperBound <= sourceUpperBound) // seedBound entirely inside sourceBound
+                {
+                    newSeedBounds.emplace_back(seedLowerBound+destOffset, seedUpperBound+destOffset);
+                    foundSeedInSource = true;
+                }
+                else if (seedLowerBound <= sourceUpperBound && seedLowerBound >= sourceLowerBound && seedUpperBound >= sourceUpperBound) // left half of seedBound inside sourceBound. If seedBound = <3,10> and sourceBound = (<1,5>,2) need to add <5,7> and <6,10> to newSeedBounds
+                {
+                    newSeedBounds.emplace_back(sourceUpperBound+1, seedUpperBound); // Part (right half) of the seedBound outside the sourceBound
+                    newSeedBounds.emplace_back(seedLowerBound + destOffset, sourceUpperBound+destOffset);
+                    foundSeedInSource = true;
+
+                }
+                else if (seedUpperBound >= sourceLowerBound && seedUpperBound <= sourceUpperBound && seedLowerBound <= sourceLowerBound) // right half of seedBound inside sourceBound. If seedBound = <3,10> and sourceBound = (<7,12>,2) need to add <3,6> and <9,12> to newSeedBounds
+                {
+                    newSeedBounds.emplace_back(seedLowerBound, sourceLowerBound-1); // Part (left half) of the seedBound outside the sourceBound
+                    newSeedBounds.emplace_back(sourceLowerBound + destOffset, seedUpperBound+destOffset);
+                    foundSeedInSource = true;
+
+                }
+
+                if (foundSeedInSource) break;
+
             }
-            if (currentSeed < closestLocation) closestLocation = currentSeed;
 
+            if(!foundSeedInSource) newSeedBounds.emplace_back(seedBound);
         }
+        seedBounds = newSeedBounds;
+    }
 
+    long long closestLocation = std::numeric_limits<long long>::max();;
+    for (const auto& sb : seedBounds)
+    {
+        if (sb.first < closestLocation) closestLocation = sb.first;
     }
 
     std::cout << closestLocation << std::endl;
@@ -138,19 +162,8 @@ void part2(const std::vector<std::string>& lines) {
 
 int main() {
     std::vector<std::string> lines = aoc_util::readFileSplitOnBlankLines("C:\\Users\\maxjo\\source\\repos\\AdventOfCode\\2023\\day5\\input.txt");
-
-    auto start = std::chrono::high_resolution_clock::now();
     part1(lines); // 340994526
-    auto end = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
-    std::cout << "Part1 Time taken: " << duration << " nanoseconds" << std::endl;
-
-
-    auto start2 = std::chrono::high_resolution_clock::now();
     part2(lines); // 52210644
-    auto end2 = std::chrono::high_resolution_clock::now();
-    auto duration2 = std::chrono::duration_cast<std::chrono::seconds>(end2 - start2).count();
-    std::cout << "Part2 Time taken: " << duration2 << " seconds" << std::endl;
 
 
     return 0;
